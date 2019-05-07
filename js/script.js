ccc484e37b2e507e94afb738ef1c8bba14a181c2
Djendo <HoBeOne@MacBook-Pro-de-Djendo-2.local>
2019-05-07 23:29:37 +0200

    modif from zero

--- js/script.js
+++ js/script.js
@@ -23,48 +23,49 @@
 	function gameLevelEcho() { document.getElementById("gameLevel").textContent = gameLevelValue };
 	gameLevelEcho();
 	
-	var x = 300; // Coordinate x-axis SpanceShip 
-	var y = 0; // Ordinate axis coordinates SpanceShip
-	var velocity = 5; // Speed SpaceFighter
+	let x = 300; // Coordinate x-axis SpanceShip 
+	let y = 0; // Ordinate axis coordinates SpanceShip
+	let velocity = 5; // Speed SpaceFighter
 	
 	// Switchs ///////////////////////////////////////////
-	var haut = false; // Switch top
-	var droite = false; // Switch right
-	var bas = false; // Switch down
-	var gauche = false; // Switch left
-	let liveSpaceShip = 20;
-
+	let haut = false; // Switch top
+	let droite = false; // Switch right
+	let bas = false; // Switch down
+	let gauche = false; // Switch left
+	let acceleration = 0;
 	let loop;
+	let loop_1;
 
     //////////////////////////////////////////////////////////////////
 	function globalLoop(){
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		
-		function spaceShip(){
-			// Move left and right
-			gauche && x >= 5 ? x -= velocity :''
-			droite && x <= 560 ? x += velocity : ''
-			
-			// Move up and down
-			haut && y >= -300 ? y -=  velocity : ''
-			bas && y <= -5 ? y += velocity : ''
-			
+		
+
+		function spaceShip(cdnX, cdnY, live){
 			ctx.beginPath();
 			ctx.fillStyle = "white";
-			ctx.fillRect(x + 11, y + 685, 5, 5);
-			ctx.fillRect(x + 8, y + 695, 12, 5);
+			ctx.fillRect(cdnX + 11, cdnY + 685, 5, 5);
+			ctx.fillRect(cdnX + 8, cdnY + 695, 12, 5);
 
 			ctx.fillStyle = "white";
-			ctx.fillRect(x + 0, y + 690, 30, 10);
+			ctx.fillRect(cdnX + 0, cdnY + 690, 30, 10);
 			ctx.fillStyle = "red"
-			ctx.fillRect(x + 4, y + 693, liveSpaceShip, 5);
-
-			
+			ctx.fillRect(cdnX + 4, cdnY + 693, live, 5);
 		}
-		spaceShip();
+
+		
+		// Move left and right
+		gauche && x >= 5 ? x -= velocity :''
+		droite && x <= 560 ? x += velocity : ''
+		
+		// // Move up and down
+		// haut && y >= -300 ? y -=  velocity : ''
+		// bas && y <= -5 ? y += velocity : ''
+
+		spaceShip(x, y, 22);
 
 		loop = window.requestAnimationFrame(globalLoop);
-
 	}
 	globalLoop();
     //////////////////////////////////////////////////////////////////
@@ -76,7 +77,6 @@
 		touche.keyCode == 38 ? haut = true :''
 		touche.keyCode == 39 ? droite = true :''
 		touche.keyCode == 40 ? bas = true :''
-		
 	};
 	// Action quand on relache la touche
 	function stopMove(touche) {
